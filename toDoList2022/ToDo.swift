5ff4043fce0563c207bffbe0ccac9d9758fec05a
susanna k <susannakhub@gmail.com>
2022-07-15 16:24:50 -0700

    woooo yay

---
+++ toDoList2022/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoList2022
+//
+//  Created by Scholar on 7/15/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = "oh no its broken"
+    var important = true
+    
+}
